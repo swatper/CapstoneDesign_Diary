6166c415d629fe1229e029c5350f4f4b3b0f66d7
Tibong69 <kgb69696969@gmail.com>
2025-03-30 04:24:46 +0900

    날씨 선택창 추가
    - 날씨 선택창 추가
    - 날씨 선택 창 리팩토리 해야함
    - 날씨 값 정의 해놓는걸로 바꿔야함

--- lib/KGB/writewindow.dart
+++ lib/KGB/writewindow.dart
@@ -9,7 +9,6 @@ class WriteWindow extends StatefulWidget {
 void onClickedBackButton() {}
 void onClickedNestButton() {}
 void onClickedCalanderButton() {}
-void onClickedWeatherButton() {}
 
 class _WriteWindow extends State<WriteWindow> {
   @override
@@ -76,11 +75,7 @@ class _WriteWindow extends State<WriteWindow> {
                     ],
                   ),
                   Spacer(),
-                  IconButton(
-                    onPressed: onClickedWeatherButton,
-                    icon: Icon(Icons.wb_sunny_sharp),
-                    iconSize: 50,
-                  ),
+                  WeatherButton(),
                 ],
               ),
               Padding(
@@ -115,6 +110,174 @@ class _WriteWindow extends State<WriteWindow> {
           ),
         ),
       ),
+    );
+  }
+}
+
+class WeatherButton extends StatefulWidget {
+  const WeatherButton({super.key});
+
+  @override
+  State<WeatherButton> createState() => _WeatherButtonState();
+}
+
+class _WeatherButtonState extends State<WeatherButton> {
+  Widget selectedWeatherIcon = Image.asset(
+    'assets/images/wd_sun.png', // 초기 이미지
+    width: 50,
+    height: 50,
+  );
+
+  void onClickedWeatherButton() async {
+    // 날씨 아이콘 선택을 위한 다이얼로그 표시
+    Widget? newIcon = await showGeneralDialog<Widget>(
+      context: context,
+      barrierDismissible: true, // 다이얼로그 외부를 클릭하면 닫힘
+      barrierLabel: MaterialLocalizations.of(context).modalBarrierDismissLabel,
+      barrierColor: Colors.black54, // 배경색
+      transitionDuration: const Duration(milliseconds: 300), // 애니메이션 지속 시간
+      pageBuilder: (
+        BuildContext context,
+        Animation<double> animation,
+        Animation<double> secondaryAnimation,
+      ) {
+        return Align(
+          alignment: Alignment.topCenter, // 다이얼로그 위치를 화면 상단으로 이동
+          child: Material(
+            color: Colors.transparent,
+            child: Container(
+              width: MediaQuery.of(context).size.width * 0.93, // 화면 너비의 60%
+              margin: const EdgeInsets.only(top: 140), // 상단 여백
+              //padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/weatherBack.png'),
+                  fit: BoxFit.contain,
+                ),
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center, // 세로 방향 중앙 정렬
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    '날씨를 선택하세요',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.brown,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    spacing: 1,
+                    children: [
+                      IconButton(
+                        icon: Image.asset(
+                          'assets/images/wd_sun.png',
+                          width: 45,
+                          height: 45,
+                        ),
+                        onPressed:
+                            () => Navigator.pop(
+                              context,
+                              Image.asset(
+                                'assets/images/wd_sun.png',
+                                width: 50,
+                                height: 50,
+                              ),
+                            ),
+                      ),
+                      IconButton(
+                        icon: Image.asset(
+                          'assets/images/wd_cloud.png',
+                          width: 45,
+                          height: 45,
+                        ),
+                        onPressed:
+                            () => Navigator.pop(
+                              context,
+                              Image.asset(
+                                'assets/images/wd_cloud.png',
+                                width: 50,
+                                height: 50,
+                              ),
+                            ),
+                      ),
+                      IconButton(
+                        icon: Image.asset(
+                          'assets/images/wd_rain.png',
+                          width: 45,
+                          height: 45,
+                        ),
+                        onPressed:
+                            () => Navigator.pop(
+                              context,
+                              Image.asset(
+                                'assets/images/wd_rain.png',
+                                width: 50,
+                                height: 50,
+                              ),
+                            ),
+                      ),
+                      IconButton(
+                        icon: Image.asset(
+                          'assets/images/wd_wind.png',
+                          width: 45,
+                          height: 45,
+                        ),
+                        onPressed:
+                            () => Navigator.pop(
+                              context,
+                              Image.asset(
+                                'assets/images/wd_wind.png',
+                                width: 50,
+                                height: 50,
+                              ),
+                            ),
+                      ),
+                      IconButton(
+                        icon: Image.asset(
+                          'assets/images/wd_snow.png',
+                          width: 45,
+                          height: 45,
+                        ),
+                        onPressed:
+                            () => Navigator.pop(
+                              context,
+                              Image.asset(
+                                'assets/images/wd_snow.png',
+                                width: 50,
+                                height: 50,
+                              ),
+                            ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+
+    // 선택된 아이콘 업데이트
+    if (newIcon != null) {
+      setState(() {
+        selectedWeatherIcon = newIcon;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: onClickedWeatherButton,
+      icon: selectedWeatherIcon, // 선택된 이미지 또는 아이콘 표시
+      iconSize: 50,
     );
   }
 }
